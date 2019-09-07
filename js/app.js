4c693246db552c03ee0b8dec03762e2608b7746d
Ted Osuch <ted@osuch.com>
2019-09-07 21:20:14 +0100

    added image function

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // console.log('JavaScript loaded');
+
   const albumTextInput = document.querySelector('#albumName')
   const genreRadioInput = document.querySelector('#albumGenre')
   const decadeSelectInput = document.querySelector('#albumDecade')
   const commentTextAreaInput = document.querySelector('#albumComments')
+  const albumItem = document.querySelector('#albumCover');
   const resultItem = document.querySelector('#album-results');
 
   const deleteAddedItems = function(event){
     resultItem.innerHTML = " "
   }
   //Submit
-
   const handleFormSubmit = function(event){
     event.preventDefault();
+    const unOrderedList = document.querySelector('#album-results')
+    const newImageItem = document.createElement('li')
+    unOrderedList.appendChild(newImageItem)
+    newImageItem.innerHTML = "<img src=\"" + event.target.albumCover.value + "\">";
     const newReadingListItem = document.createElement('li')
-    const unOrderedList = document.querySelector('ul')
     unOrderedList.appendChild(newReadingListItem)
     newReadingListItem.classList.add('red')
-    newReadingListItem.textContent = `
-    ${event.target.albumName.value} -
-    ${event.target.albumGenre.value} -
-    ${event.target.albumDecade.value} -
-    ${event.target.albumComments.value}`;
+    newReadingListItem.innerHTML = "<span class=\"global\">" + "<strong>" +
+    event.target.albumName.value + "</strong> - " +
+    event.target.albumGenre.value + " - " +
+    event.target.albumDecade.value + " - " +
+    event.target.albumComments.value + "</span>";
+
     document.getElementById("new-item-form").reset();
   }
-
+// listeners
   const form = document.querySelector('#new-item-form')
   form.addEventListener('submit', handleFormSubmit)
 
   const deleteResults = document.querySelector('#delete-all')
   deleteResults.addEventListener('click', deleteAddedItems)
-
 })
